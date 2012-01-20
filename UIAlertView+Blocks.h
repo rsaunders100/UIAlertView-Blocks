74c7a450aef1b6fea40f75bbf40edb8f8fb76c03
Robert Saunders <rsaunders100@gmail.com>
2012-01-20 22:20:11 +0000

    Fleshed out two basic implementations for my version of UIAlertView+Blocks
    I only include two methods,  a one button alert and a two button alert.  This is plenty - I never seam to use anything else in my code.  Including more methods would slightly obfuscate the interface.

--- UIAlertView+Blocks.h
+++ UIAlertView+Blocks.h
@@ -4,15 +4,25 @@
 //
 //  Created by Jiva DeVoe on 12/28/10.
 //  Copyright 2010 Random Ideas, LLC. All rights reserved.
+//  Modified by Robert Saunders on 20/01/12
 //
 
 #import <Foundation/Foundation.h>
-#import "RIButtonItem.h"
 
 @interface UIAlertView (Blocks)
 
--(id)initWithTitle:(NSString *)inTitle message:(NSString *)inMessage cancelButtonItem:(RIButtonItem *)inCancelButtonItem otherButtonItems:(RIButtonItem *)inOtherButtonItems, ... NS_REQUIRES_NIL_TERMINATION;
 
-- (NSInteger)addButtonItem:(RIButtonItem *)item;
+-(id)initWithTitle:(NSString *)inTitle 
+           message:(NSString *)inMessage
+  cancelButtonTitle:(NSString *)inCancelButtonTitle
+cancelButtonAction:(void (^)(void))inCancelAction;
+
+-(id)initWithTitle:(NSString *)inTitle 
+           message:(NSString *)inMessage
+  cancelButtonTitle:(NSString *)inCancelButtonTitle
+cancelButtonAction:(void (^)(void))inCancelAction
+  otherButtonTitle:(NSString*)inOtherButtonTitle
+otherButtonAction:(void (^)(void))inOtherButtonAction;
+
 
 @end
--- UIAlertView+Blocks.m
+++ UIAlertView+Blocks.m
@@ -4,68 +4,77 @@
 //
 //  Created by Jiva DeVoe on 12/28/10.
 //  Copyright 2010 Random Ideas, LLC. All rights reserved.
+//  Modified by Robert Saunders on 20/01/12
 //
 
 #import "UIAlertView+Blocks.h"
 #import <objc/runtime.h>
 
-static NSString *RI_BUTTON_ASS_KEY = @"com.random-ideas.BUTTONS";
+static NSString *CANCEL_ACTION_ASS_KEY = @"com.robsaunders.cancelbuttonaction";
+static NSString *OTHER_ACTION_ASS_KEY = @"com.robsaunders.otherbuttonaction";
 
 @implementation UIAlertView (Blocks)
 
--(id)initWithTitle:(NSString *)inTitle message:(NSString *)inMessage cancelButtonItem:(RIButtonItem *)inCancelButtonItem otherButtonItems:(RIButtonItem *)inOtherButtonItems, ... 
+-(id)initWithTitle:(NSString *)inTitle 
+           message:(NSString *)inMessage
+ cancelButtonTitle:(NSString *)inCancelButtonTitle
+cancelButtonAction:(void (^)(void))inCancelAction
 {
-    if((self = [self initWithTitle:inTitle message:inMessage delegate:self cancelButtonTitle:inCancelButtonItem.label otherButtonTitles:nil]))
+    if((self = [self initWithTitle:inTitle 
+                           message:inMessage 
+                          delegate:self
+                 cancelButtonTitle:inCancelButtonTitle
+                  otherButtonTitles:nil]))
     {
-        NSMutableArray *buttonsArray = [NSMutableArray array];
-        
-        RIButtonItem *eachItem;
-        va_list argumentList;
-        if (inOtherButtonItems)                     
-        {                                  
-            [buttonsArray addObject: inOtherButtonItems];
-            va_start(argumentList, inOtherButtonItems);       
-            while((eachItem = va_arg(argumentList, RIButtonItem *))) 
-            {
-                [buttonsArray addObject: eachItem];            
-            }
-            va_end(argumentList);
-        }    
-        
-        for(RIButtonItem *item in buttonsArray)
-        {
-            [self addButtonWithTitle:item.label];
-        }
-        
-        if(inCancelButtonItem)
-            [buttonsArray insertObject:inCancelButtonItem atIndex:0];
-        
-        objc_setAssociatedObject(self, RI_BUTTON_ASS_KEY, buttonsArray, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        
-        [self setDelegate:self];
-        [self retain]; // keep yourself around!
+      objc_setAssociatedObject(self, CANCEL_ACTION_ASS_KEY, inCancelAction, OBJC_ASSOCIATION_COPY_NONATOMIC);
+      [self setDelegate:self];
+      [self retain]; // keep yourself around!
     }
     return self;
 }
 
-- (NSInteger)addButtonItem:(RIButtonItem *)item
-{	
-    NSMutableArray *buttonsArray = objc_getAssociatedObject(self, RI_BUTTON_ASS_KEY);	
-	
-	NSInteger buttonIndex = [self addButtonWithTitle:item.label];
-	[buttonsArray addObject:item];
-	
-	return buttonIndex;
+-(id)initWithTitle:(NSString *)     inTitle 
+           message:(NSString *)     inMessage
+ cancelButtonTitle:(NSString *)     inCancelButtonTitle
+cancelButtonAction:(void (^)(void)) inCancelButtonAction
+  otherButtonTitle:(NSString*)      inOtherButtonTitle
+ otherButtonAction:(void (^)(void)) inOtherButtonAction 
+{
+  if((self = [self initWithTitle:inTitle 
+                         message:inMessage 
+                        delegate:self
+               cancelButtonTitle:inCancelButtonTitle
+               otherButtonTitles:inOtherButtonTitle,nil]))
+  {
+    objc_setAssociatedObject(self, CANCEL_ACTION_ASS_KEY, inCancelButtonAction, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    objc_setAssociatedObject(self, OTHER_ACTION_ASS_KEY, inOtherButtonAction, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    
+    [self setDelegate:self];
+    [self retain]; // keep yourself around!
+  }
+  return self;
 }
+
+
 
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
 {
-    NSArray *buttonsArray = objc_getAssociatedObject(self, RI_BUTTON_ASS_KEY);
-    RIButtonItem *item = [buttonsArray objectAtIndex:buttonIndex];
-    if(item.action)
-        item.action();
-    objc_setAssociatedObject(self, RI_BUTTON_ASS_KEY, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    [self release]; // and release yourself!
+  void (^action)(void);
+  
+  if (buttonIndex == 0) 
+  {
+    action  = objc_getAssociatedObject(self, CANCEL_ACTION_ASS_KEY);
+  } 
+  else if (buttonIndex == 1)
+  {
+    action  = objc_getAssociatedObject(self, OTHER_ACTION_ASS_KEY);
+  }
+  if (action) action();
+  
+  objc_setAssociatedObject(self, CANCEL_ACTION_ASS_KEY, nil, OBJC_ASSOCIATION_COPY);
+  objc_setAssociatedObject(self, OTHER_ACTION_ASS_KEY, nil, OBJC_ASSOCIATION_COPY);
+  
+  [self release]; // and release yourself!
 }
 
 @end
